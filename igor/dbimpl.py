3340fd2839eb7d6c4c256911a000bc9ab9d927c2
Jack Jansen <Jack.Jansen@cwi.nl>
2016-01-21 21:37:25 +0000

    Added locking to the database

--- igor/dbimpl.py
+++ igor/dbimpl.py
@@ -18,7 +18,6 @@ class DBKeyError(KeyError):
 class DBParamError(ValueError):
 	pass
 	
-	
 class DBSerializer:
 	"""Baseclass with methods to provide a mutex and a condition variable"""
 	def __init__(self):	  
@@ -33,6 +32,12 @@ class DBSerializer:
 		
 	def leave(self):
 		self._lock.release()
+		
+	def __enter__(self):
+		self.enter()
+		
+	def __exit__(self, *args):
+		self.leave()
 
 	def waitLocation(self, location, oldValue=None):
 		"""Register that we are interested in the given XPath. Returns the semaphore"""
@@ -70,31 +75,37 @@ class DBImpl(DBSerializer):
 			self.initialize(filename=filename)
 		else:
 			self.initialize(xmlstring=DOCUMENT)
-		
+
 	def signalNodelist(self, nodelist):
-		newFilename = self.filename + '.NEW'
-		self._doc.writexml(open(newFilename, 'w'))
-		os.rename(newFilename, self.filename)
-		DBSerializer.signalNodelist(self, nodelist)
+		with self:
+			newFilename = self.filename + '.NEW'
+			self._doc.writexml(open(newFilename, 'w'))
+			print 'xxxjack rename', newFilename, 'to', self.filename
+			os.rename(newFilename, self.filename)
+			DBSerializer.signalNodelist(self, nodelist)
 		
 	def getMessageCount(self):
-		return self.lockCount
+		with self:
+			return self.lockCount
 		
 	def terminate(self):
-		self._terminating = True
-		sys.exit(1)
+		with self:
+			self._terminating = True
+			sys.exit(1)
 		
 	def is_terminating(self):
-		return self._terminating
+		with self:
+			return self._terminating
 		
 	def initialize(self, xmlstring=None, filename=None):
 		"""Reset the document to a known value (passed as an XML string"""
-		if filename:
-			self._doc = xml.dom.minidom.parse(filename)
-		elif xmlstring:
-			self._doc = xml.dom.minidom.parseString(xmlstring)
-		else:
-			self._doc = self._domimpl.createDocument('', 'root', None)
+		with self:
+			if filename:
+				self._doc = xml.dom.minidom.parse(filename)
+			elif xmlstring:
+				self._doc = xml.dom.minidom.parseString(xmlstring)
+			else:
+				self._doc = self._domimpl.createDocument('', 'root', None)
 	
 	def echo(self, arg):
 		"""Return the argument (for performance testing)"""
@@ -102,12 +113,14 @@ class DBImpl(DBSerializer):
 		
 	def getXMLDocument(self):
 		"""Return the whole document (as an XML string)"""
-		rv = self._doc.toprettyxml()
-		return rv
+		with self:
+			rv = self._doc.toprettyxml()
+			return rv
 		
 	def getDocument(self):
 		"""Return the whole document (as a DOM element)"""
-		return self._doc.documentElement
+		with self:
+			return self._doc.documentElement
 		
 	def splitXPath(self, location):
 		lastSlashPos = location.rfind('/')
@@ -183,31 +196,32 @@ class DBImpl(DBSerializer):
 		return t, v
 
 	def elementFromTagAndData(self, tag, data):
-		newnode = self._doc.createElement(tag)
-		if not isinstance(data, dict):
-			# Not key/value, so a raw value. Convert to something string-like
-			data = unicode(data)
-			newnode.appendChild(self._doc.createTextNode(data))
+		with self:
+			newnode = self._doc.createElement(tag)
+			if not isinstance(data, dict):
+				# Not key/value, so a raw value. Convert to something string-like
+				data = unicode(data)
+				newnode.appendChild(self._doc.createTextNode(data))
+				return newnode
+			for k, v in data.items():
+				if k == '#text':
+					if not isinstance(v, list):
+						v = [v]
+					for string in v:
+						newtextnode = self._doc.createTextNode(string)
+						newnode.appendChild(newtextnode)
+				elif k and k[0] == '@':
+					attrname = k[1:]
+					newattr = self._doc.createAttribute(attrname)
+					newattr.value = v
+					newnode.appendChild(newattr)
+				else:
+					if not isinstance(v, list):
+						v = [v]
+					for childdef in v:
+						newchild = self.elementFromTagAndData(k, childdef)
+						newnode.appendChild(newchild)
 			return newnode
-		for k, v in data.items():
-			if k == '#text':
-				if not isinstance(v, list):
-					v = [v]
-				for string in v:
-					newtextnode = self._doc.createTextNode(string)
-					newnode.appendChild(newtextnode)
-			elif k and k[0] == '@':
-				attrname = k[1:]
-				newattr = self._doc.createAttribute(attrname)
-				newattr.value = v
-				newnode.appendChild(newattr)
-			else:
-				if not isinstance(v, list):
-					v = [v]
-				for childdef in v:
-					newchild = self.elementFromTagAndData(k, childdef)
-					newnode.appendChild(newchild)
-		return newnode
 		
 	def elementFromXML(self, xmltext):
 		newdoc = xml.dom.minidom.parseString(xmltext)
@@ -215,160 +229,172 @@ class DBImpl(DBSerializer):
 		
 	def setValue(self, location, value):
 		"""Set (or insert) a single node by a given value (passed as a string)"""
-		
-		# Find the node, if it exists.
-		node = xpath.findnode(location, self._doc.documentElement)
-		if node is None:
-			# Does not exist yet. Try to add it.
-			parent, child = self.splitXPath(location)
-			if not parent or not child:
-				raise DBKeyError("XPath %s does not refer to unique new or existing location" % location)
-			return self.newValue(parent, 'child', child, value)
+		with self:
+			# Find the node, if it exists.
+			node = xpath.findnode(location, self._doc.documentElement)
+			if node is None:
+				# Does not exist yet. Try to add it.
+				parent, child = self.splitXPath(location)
+				if not parent or not child:
+					raise DBKeyError("XPath %s does not refer to unique new or existing location" % location)
+				return self.newValue(parent, 'child', child, value)
 
-		# Sanity check
-		if node.nodeType == node.DOCUMENT_NODE:
-			raise DBParamError('Cannot replace value of /')
+			# Sanity check
+			if node.nodeType == node.DOCUMENT_NODE:
+				raise DBParamError('Cannot replace value of /')
 			
-		# Clear out old contents of the node
-		while node.firstChild: node.removeChild(node.firstChild)
+			# Clear out old contents of the node
+			while node.firstChild: node.removeChild(node.firstChild)
 		
-		if hasattr(value, 'nodeType'):
-			# It seems to be a DOM node. Insert it.
-			node.appendChild(value)
-		else:
-			# Insert the new text value
-			node.appendChild(self._doc.createTextNode(str(value)))
+			if hasattr(value, 'nodeType'):
+				# It seems to be a DOM node. Insert it.
+				node.appendChild(value)
+			else:
+				# Insert the new text value
+				node.appendChild(self._doc.createTextNode(str(value)))
 		
-		# Signal anyone waiting
-		self.signalNodelist([node])
+			# Signal anyone waiting
+			self.signalNodelist([node])
 		
-		# Return the location of the new node
-		return getXPathForElement(node)
+			# Return the location of the new node
+			return getXPathForElement(node)
 		
 	def newValue(self, location, where, name, value):
 		"""Insert a single new node into the document (value passed as a string)"""
+		with self:
+			# Create the new node to be instered
+			newnode = self._doc.createElement(name)
+			newnode.appendChild(self._doc.createTextNode(str(value)))
 		
-		# Create the new node to be instered
-		newnode = self._doc.createElement(name)
-		newnode.appendChild(self._doc.createTextNode(str(value)))
-		
-		# Find the node that we want to insert it relative to
-		node = xpath.findnode(location, self._doc.documentElement)
-		if node is None:
-			raise DBKeyError(location)
+			# Find the node that we want to insert it relative to
+			node = xpath.findnode(location, self._doc.documentElement)
+			if node is None:
+				raise DBKeyError(location)
 			
-		# Insert it in the right place
-		if where == 'before':
-			node.parentNode.insertBefore(node, newnode)
-		elif where == 'after':
-			newnode.nextSibling = node.nextSibling
-			node.nextSibling = newnode
-		elif where == 'child':
-			node.appendChild(newnode)
-		else:
-			raise DBParamError('where must be before, after or child')
+			# Insert it in the right place
+			if where == 'before':
+				node.parentNode.insertBefore(node, newnode)
+			elif where == 'after':
+				newnode.nextSibling = node.nextSibling
+				node.nextSibling = newnode
+			elif where == 'child':
+				node.appendChild(newnode)
+			else:
+				raise DBParamError('where must be before, after or child')
 
-		# Signal anyone waiting
-		self.signalNodelist([newnode, newnode.parentNode])
+			# Signal anyone waiting
+			self.signalNodelist([newnode, newnode.parentNode])
 		
-		# Return the location of the new node
-		return getXPathForElement(newnode)
+			# Return the location of the new node
+			return getXPathForElement(newnode)
 		
 	def delValue(self, location):
 		"""Remove a single node from the document"""
-		node = xpath.findnode(location, self._doc.documentElement)
-		if node is None:
-			raise DBKeyError(location)
-		parentNode = node.parentNode
-		parentNode.removeChild(node)
+		with self:
+			node = xpath.findnode(location, self._doc.documentElement)
+			if node is None:
+				raise DBKeyError(location)
+			parentNode = node.parentNode
+			parentNode.removeChild(node)
 		
-		# Signal anyone waiting
-		self.signalNodelist([parentNode])
+			# Signal anyone waiting
+			self.signalNodelist([parentNode])
 		
 	def delValues(self, location):
 		"""Remove a (possibly empty) set of nodes from the document"""
-		nodelist = xpath.find(location, self._doc.documentElement)
-		parentList = []
-		print 'xxxjack delValues', repr(nodelist)
-		for node in nodelist:
-			parentNode = node.parentNode
-			parentNode.removeChild(node)
-			if not parentNode in parentList:
-				parentList.append(parentNode)
-		self.signalNodelist(parentList)
+		with self:
+			nodelist = xpath.find(location, self._doc.documentElement)
+			parentList = []
+			print 'xxxjack delValues', repr(nodelist)
+			for node in nodelist:
+				parentNode = node.parentNode
+				parentNode.removeChild(node)
+				if not parentNode in parentList:
+					parentList.append(parentNode)
+			self.signalNodelist(parentList)
 			
 	def hasValue(self, location):
 		"""Return xpath if the location exists, None otherwise"""
-		node = xpath.findnode(location, self._doc.documentElement)
-		if node:
-			return getXPathForElement(node)
-		return None
+		with self:
+			node = xpath.findnode(location, self._doc.documentElement)
+			if node:
+				return getXPathForElement(node)
+			return None
 		
 	def waitValue(self, location):
-		node = xpath.findnode(location, self._doc.documentElement)
-		if not node:
-			self.waitLocation(location)
+		with self:
 			node = xpath.findnode(location, self._doc.documentElement)
-			assert node
-		return getXPathForElement(node)
+			if not node:
+				self.waitLocation(location)
+				node = xpath.findnode(location, self._doc.documentElement)
+				assert node
+			return getXPathForElement(node)
 
 	def hasValues(self, location):
 		"""Return a list of xpaths for the given location"""
-		nodelist = xpath.find(location, self._doc.documentElement)
-		return map(getXPathForElement, nodelist)
+		with self:
+			nodelist = xpath.find(location, self._doc.documentElement)
+			return map(getXPathForElement, nodelist)
 		
 	def getValue(self, location):
 		"""Return a single value from the document (as string)"""
-		return xpath.findvalue(location, self._doc.documentElement)
+		with self:
+			return xpath.findvalue(location, self._doc.documentElement)
 		
 	def getValues(self, location):
 		"""Return a list of node values from the document (as names and strings)"""
-		nodelist = xpath.find(location, self._doc.documentElement)
-		return self._getValueList(nodelist)
+		with self:
+			nodelist = xpath.find(location, self._doc.documentElement)
+			return self._getValueList(nodelist)
 		
 	def getElements(self, location):
 		"""Return a list of DOM nodes (elements only, for now) that match the location"""
-		nodeList = xpath.find(location, self._doc.documentElement)
-		return nodeList
+		with self:
+			nodeList = xpath.find(location, self._doc.documentElement)
+			return nodeList
 		
 	def _getValueList(self, nodelist):
-		rv = []
-		for node in nodelist:
-			rv.append((getXPathForElement(node), xpath.expr.string_value(node)))
-		return rv
+		with self:
+			rv = []
+			for node in nodelist:
+				rv.append((getXPathForElement(node), xpath.expr.string_value(node)))
+			return rv
 		
 	def pullValue(self, location):
 		"""Wait for a value, remove it from the document, return it (as string)"""
-		node = xpath.findnode(location, self._doc.documentElement)
-		if not node:
-			self.waitLocation(location)
+		with self:
 			node = xpath.findnode(location, self._doc.documentElement)
-			assert node
-		rv = xpath.expr.string_value(node)
-		node.parentNode.removeChild(node)
-		self.signalNodelist([parentnode])
-		return rv
+			if not node:
+				self.waitLocation(location)
+				node = xpath.findnode(location, self._doc.documentElement)
+				assert node
+			rv = xpath.expr.string_value(node)
+			node.parentNode.removeChild(node)
+			self.signalNodelist([parentnode])
+			return rv
   
 	def pullValues(self, location):
 		"""Wait for values, remove them from the document, return it (as list of strings)"""
-		nodelist = xpath.find(location, self._doc.documentElement)
-		if not nodelist:
-			self.waitLocation(location)
+		with self:
 			nodelist = xpath.find(location, self._doc.documentElement)
-		assert nodelist
-		rv = self._getValueList(nodelist)
-		parentList = []
-		for node in nodelist:
-			parentNode = node.parentNode
-			parentNode.removeChild(node)
-			if not parentNode in parentList:
-				parentList.append(parentNode)
-		self.signalNodelist(parentList)
-		return rv
+			if not nodelist:
+				self.waitLocation(location)
+				nodelist = xpath.find(location, self._doc.documentElement)
+			assert nodelist
+			rv = self._getValueList(nodelist)
+			parentList = []
+			for node in nodelist:
+				parentNode = node.parentNode
+				parentNode.removeChild(node)
+				if not parentNode in parentList:
+					parentList.append(parentNode)
+			self.signalNodelist(parentList)
+			return rv
 		
 	def trackValue(self, location, generation):
 		"""Generator. Like waitValue, but keeps on returning changed paths"""
-		generation = self.waitLocation(location, generation)
-		node = xpath.findnode(location, self._doc.documentElement)
-		assert node
-		return getXPathForElement(node), generation
+		with self:
+			generation = self.waitLocation(location, generation)
+			node = xpath.findnode(location, self._doc.documentElement)
+			assert node
+			return getXPathForElement(node), generation

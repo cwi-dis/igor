0c849ff5a3a813e5583f20310778841b243a07c6
Unknown DIS group member <dis-list@cwi.nl>
2016-05-25 13:00:41 +0000

    Streamlining installation process

--- igor/setup.py
+++ igor/setup.py
@@ -103,7 +103,7 @@ setup(
     # pip to create the appropriate form of executable for the target platform.
     entry_points={
         'console_scripts': [
-            'sample=sample:main',
+            'homeServer=homeServer.__main__',
         ],
     },
 )

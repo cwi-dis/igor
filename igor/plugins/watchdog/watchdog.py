b55302d3a3458c5451cb4a247d5e2b6f3b185d42
Unknown DIS group member <dis-list@cwi.nl>
2016-06-14 16:42:12 +0200

    Use thread to feed the dog

--- igor/plugins/watchdog/watchdog.py
+++ igor/plugins/watchdog/watchdog.py
@@ -1,10 +1,16 @@
 """Linux kernel watchdog support"""
+import time
+import thread
 
 watchdog_device = None
+watchdog_timeout = 60
+stop_feeding = time.time() + watchdog_timeout
 
 def watchdog(timeout=None, device='/dev/watchdog'):
     """Initialize and/or feed the watchdog"""
     global watchdog_device
+    global watchdog_timeout
+    global stop_feeding
     # Close the watchdog, if that is what is wanted
     if timeout == 0:
         if watchdog_device:
@@ -16,12 +22,20 @@ def watchdog(timeout=None, device='/dev/watchdog'):
     if not watchdog_device:
         import watchdogdev
         watchdog_device = watchdogdev.watchdog(device)
+        thread.start_new_thread(_feeder, ())
         rv += "watchdog opened\n"
     # Set the timeout, if needed
     if timeout:
-        watchdog_device.set_timeout(int(timeout))
+        watchdog_timeout = int(timeout)
         rv += "watchdog timeout set to %d\n" % timeout
     # Feed the dog
     watchdog_device.write('\n')
+    stop_feeding = time.time+watchdog_timeout
     rv += "watchdog fed\n"
     return rv
+
+def _feeder():
+    while watchdog_device:
+        if time.time() < stop_feeding:
+            watchdog_device.write('\n')
+        time.sleep(2)

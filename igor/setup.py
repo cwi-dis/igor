a751dd0bc234565d2546449b8d57ed44e4671934
Jack Jansen <Jack.Jansen@cwi.nl>
2016-07-06 15:28:04 +0200

    Minor fixes

--- igor/setup.py
+++ igor/setup.py
@@ -74,7 +74,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=["httplib2", "py-dom-xpath", "requests", "web.py", "python-dateutil"],
+    install_requires=["httplib2", "requests", "web.py", "python-dateutil", "py-dom-xpath-redux"],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,

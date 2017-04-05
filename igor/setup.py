71ac058090252d36a9d5358aa0f38b4b30d3ee29
Jack Jansen <Jack.Jansen@cwi.nl>
2017-04-05 18:43:01 +0200

    Moving more stuff into the package

--- igor/setup.py
+++ igor/setup.py
@@ -89,8 +89,7 @@ setup(
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
     package_data={
-        'igor': ['static/*', 'templates/*'],
-        'empty': ['igorDatabase.empty/*'],
+        'igor': ['static/*', 'template/*', 'igorDatabase.empty/*'],
     },
     include_package_data=True,
     # Although 'package_data' is the preferred approach, in some case you may

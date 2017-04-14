119a93008fb48be96f568713d10ff4de30907426
Jack Jansen <Jack.Jansen@cwi.nl>
2017-04-14 13:50:50 +0200

    Moved dhcpleases.py here

--- igor/plugins/dhcp/scripts/pull.sh
+++ igor/plugins/dhcp/scripts/pull.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 dir=`dirname $0`
-dirDHCP=~jack/src/dis/jack/home
 
-python $dirDHCP/dhcpleases.py --json --wrap --ping --arp -i client-hostname -i hardware -i ip-address -i alive -i arp -i ping | igorVar --put application/json sensors/dhcp
+python $dir/dhcpleases.py --json --wrap --ping --arp -i client-hostname -i hardware -i ip-address -i alive -i arp -i ping | igorVar --put application/json sensors/dhcp

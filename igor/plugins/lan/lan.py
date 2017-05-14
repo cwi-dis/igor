ccc991347a5a556081daed1a883fc0e3d1ab15e5
Jack Jansen <Jack.Jansen@cwi.nl>
2017-05-14 23:40:55 +0200

    Bug fixes

--- igor/plugins/lan/lan.py
+++ igor/plugins/lan/lan.py
@@ -24,26 +24,26 @@ def lan(name=None, service='services/%s', ip=None, port=80, timeout=5):
             msg = "502 plugin did not have DATABASE_ACCESS set"
             raise web.HTTPError(msg, {"Content-type": "text/plain"}, msg+'\n\n')
         try:
-        	oldValue = DATABASE_ACCESS.get_key(service, 'text/plain', None)
+            oldValue = DATABASE_ACCESS.get_key(service, 'text/plain', None)
         except web.HTTPError:
-        	web.ctx.status = "200 OK"
-        	oldValue = 'rabarber'
-		if oldValue != repr(alive):
-			try:
-				rv = DATABASE_ACCESS.put_key(service + '/alive', 'text/plain', None, repr(alive), 'text/plain', replace=True)
-			except web.HTTPError:
-				raise myWebError("501 Failed to store into %s" % service)
-			if alive:
-			    # If the service is alive we delete any error message and we also reset the "ignore errors" indicator
-			    try:
-    			    DATABASE_ACCESS.delete_key(service + '/errorMessage')
-    			except web.HTTPError:
-    			    pass
-			    try:
-    			    DATABASE_ACCESS.delete_key(service + '/ignoreErrorUntil')
-    			except web.HTTPError:
-    			    pass
-			else:
-			    # If the service is not alive we set an error message
-			    DATABASE_ACCESS.put_key(service + '/errorMessage', 'text/plain', None, "%s is not available" % name, replace=True)
+            web.ctx.status = "200 OK"
+            oldValue = 'rabarber'
+        if oldValue != repr(alive):
+            try:
+                rv = DATABASE_ACCESS.put_key(service + '/alive', 'text/plain', None, repr(alive), 'text/plain', replace=True)
+            except web.HTTPError:
+                raise myWebError("501 Failed to store into %s" % service)
+            if alive:
+                # If the service is alive we delete any error message and we also reset the "ignore errors" indicator
+                try:
+                    DATABASE_ACCESS.delete_key(service + '/errorMessage')
+                except web.HTTPError:
+                    pass
+                try:
+                    DATABASE_ACCESS.delete_key(service + '/ignoreErrorUntil')
+                except web.HTTPError:
+                    pass
+            else:
+                # If the service is not alive we set an error message
+                DATABASE_ACCESS.put_key(service + '/errorMessage', 'text/plain', None, "%s is not available" % name, 'text/plain', replace=True)
     return repr(alive)

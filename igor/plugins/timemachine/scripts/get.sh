2ca6396c3543afe210d27e6ac4f52e287e689005
Jack Jansen <Jack.Jansen@cwi.nl>
2017-05-29 00:18:09 +0200

    Better error message

--- igor/plugins/timemachine/scripts/get.sh
+++ igor/plugins/timemachine/scripts/get.sh
@@ -15,9 +15,10 @@ esac
 backupFilename=`$tmutil latestbackup`
 if [ $? -ne 0 ]; then
 	igorVar --put text/plain --data "" services/$igor_name/alive
-	igorVar --put text/plain --data "$backupFilename" services/$igor_name/errorMessage
+	igorVar --put text/plain --data "$igor_name status cannot be determined: $backupFilename" services/$igor_name/errorMessage
 else
 	igorVar --put text/plain --data "true" services/$igor_name/alive
 	backupTime=`echo "$backupFilename" | sed -e 's@.*/\([0-9][[0-9][0-9][0-9]\)-\([0-9][0-9]\)-\([0-9][0-9]\)-\([0-9][0-9]\)\([0-9][0-9]\)\([0-9][0-9]\)@\1-\2-\3 \4:\5:\6@'`
 	igorVar --put text/plain --data "$backupTime" services/$igor_name/lastActivity
+	igorVar --delete services/$igor_name/errorMessage
 fi

eae0293b0aceb0f8bfe953f6c14edb333a7f144d
Jack Jansen <Jack.Jansen@cwi.nl>
2017-08-16 22:36:03 +0200

    Added support for espeak, flite and festival tts on Linux

--- igor/plugins/say/scripts/message.sh
+++ igor/plugins/say/scripts/message.sh
@@ -2,11 +2,29 @@
 if [ -x /usr/libexec/path_helper ]; then
 	eval `/usr/libexec/path_helper -s`
 fi
+case `which say` in
+*say)
+	;;
+*)
+	say() {
+		if which espeak; then
+			espeak "$*"
+		elif which flite; then
+			flite "$*"
+		elif which festival; then
+			echo "$*" | festival --tts
+		else
+			echo "For speech output Install speak, flite or festival if you do not have say"
+			exit 1
+		fi
+	}
+esac
 case x$igor_remoteHost in
 x)
 	say=say
 	;;
 *)
+	# Remote assumes OSX, for now....
 	say="ssh $igor_remoteHost say"
 	;;
 esac
@@ -18,4 +36,4 @@ x*)
 	sayArg="-v $igor_voice"
 	;;
 esac
-$say $sayArg $@
+$say $sayArg "$@"

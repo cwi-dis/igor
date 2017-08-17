3466fac6e5201635ed589650791b061b1d726270
Jack Jansen <Jack.Jansen@cwi.nl>
2017-08-17 22:05:50 +0200

    Fixed buienradar URL

--- igor/plugins/buienradar/scripts/pull.sh
+++ igor/plugins/buienradar/scripts/pull.sh
@@ -6,4 +6,5 @@ dir=`dirname $0`
 lat=`expr $igor_lat : '\([0-9]*\.[0-9][0-9]\)[0-9]*'`
 lon=`expr $igor_lon : '\([0-9]*\.[0-9][0-9]\)[0-9]*'`
 
-python $dir/parseBuienradar.py "http://gpsgadget.buienradar.nl/data/raintext?lat=$lat&lon=$lon"  | igorVar --put application/json sensors/buienradar
+python $dir/parseBuienradar.py "https://gps.buienradar.nl/getrr.php?lat=$lat&lon=$lon"  | igorVar --put application/json sensors/buienradar
+https://gps.buienradar.nl/getrr.php?lat=51&lon=3

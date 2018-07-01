94d194efa449a3ca953199f146e0d39b75fafe3e
Jack Jansen <Jack.Jansen@cwi.nl>
2018-07-01 22:45:13 +0200

    Adapted for allowing https in various places

--- igor/plugins/plant/scripts/pull.sh
+++ igor/plugins/plant/scripts/pull.sh
@@ -2,5 +2,5 @@
 if [ -x /usr/libexec/path_helper ]; then
 	eval `/usr/libexec/path_helper -s`
 fi
-plantURL=http://plant.local/stepper/
+plantURL=$igor_protocol://$igor_host/stepper/
 igorVar --url $plantURL 0 | igorVar --put application/json --checknonempty devices/plant

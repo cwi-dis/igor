81d629bdc2d04548662bedb8276af748ca578d48
Jack Jansen <Jack.Jansen@cwi.nl>
2018-07-15 23:57:58 +0200

    Add tokens for new users

--- igor/plugins/user/user.py
+++ igor/plugins/user/user.py
@@ -21,43 +21,60 @@ class UserPlugin:
     def index(self, token=None):
         raise web.notfound()
     
-    def add(self, token=None, name=None, password=None, returnTo=None):
-        if not NAME_RE.match(name):
+    def add(self, token=None, username=None, password=None, returnTo=None):
+        if not NAME_RE.match(username):
             raise myWebError('400 Illegal name for user')
-        if DATABASE_ACCESS.get_key('identities/%s' % name, 'application/x-python-object', 'multi', token):
+        if DATABASE_ACCESS.get_key('identities/%s' % username, 'application/x-python-object', 'multi', token):
             raise myWebError('400 user already exists')
         # Create identities item
-        DATABASE_ACCESS.put_key('identities/%s' % name, 'text/plain', 'ref', '', 'text/plain', token, replace=True)
+        DATABASE_ACCESS.put_key('identities/%s' % username, 'text/plain', 'ref', '', 'text/plain', token, replace=True)
         # Create people item
-        DATABASE_ACCESS.put_key('people/%s' % name, 'text/plain', 'ref', '', 'text/plain', token, replace=True)
+        DATABASE_ACCESS.put_key('people/%s' % username, 'text/plain', 'ref', '', 'text/plain', token, replace=True)
         # Create password
-        COMMANDS.accessControl('setUserPassword', token=token, username=name, password=password)
+        COMMANDS.accessControl('setUserPassword', token=token, username=username, password=password)
         # Create capabilities
-        # xxxjack
+        COMMANDS.accessControl('newToken', 
+            token=token, 
+            tokenId='admin-data',
+            newOwner='identities/%s' % username, 
+            newPath='/data/identities/%s' % username,
+            get='descendant-or-self', 
+            put='descendant', 
+            post='descendant', 
+            delete='descendant')
+        COMMANDS.accessControl('newToken', 
+            token=token, 
+            tokenId='admin-data',
+            newOwner='identities/%s' % username, 
+            newPath='/data/people/%s' % username, 
+            put='descendant', 
+            post='descendant', 
+            delete='descendant')
         COMMANDS.save(token)
         if returnTo:
             raise web.seeother(returnTo)
         return ''
         
-    def delete(self, token=None, name=None, returnTo=None):
-        if not NAME_RE.match(name):
+    def delete(self, token=None, username=None, returnTo=None):
+        if not NAME_RE.match(username):
             raise myWebError('400 Illegal name for user')
-        if not DATABASE_ACCESS.get_key('identities/%s' % name, 'application/x-python-object', 'multi', token):
-            raise myWebError('404 user %s does not exist' % name)
-        DATABASE_ACCESS.delete_key('people/%s' % name, token)
-        # xxxjack delete or save all capabilities
-        DATABASE_ACCESS.delete_key('identities/%s' % name, token)
+        if not DATABASE_ACCESS.get_key('identities/%s' % username, 'application/x-python-object', 'multi', token):
+            raise myWebError('404 user %s does not exist' % username)
+        DATABASE_ACCESS.delete_key('people/%s' % username, token)
+        # delete or save all capabilities
+        # xxxjack to be implemented...
+        DATABASE_ACCESS.delete_key('identities/%s' % username, token)
         COMMANDS.save(token)
         if returnTo:
             raise web.seeother(returnTo)
         return ''
         
-    def password(self, token=None, name=None, password=None, returnTo=None):
-        if not NAME_RE.match(name):
+    def password(self, token=None, username=None, password=None, returnTo=None):
+        if not NAME_RE.match(username):
             raise myWebError('400 Illegal name for user')
-        if not DATABASE_ACCESS.get_key('identities/%s' % name, 'application/x-python-object', 'multi', token):
-            raise myWebError('404 user %s does not exist' % name)
-        COMMANDS.accessControl('setUserPassword', token=token, username=name, password=password)
+        if not DATABASE_ACCESS.get_key('identities/%s' % username, 'application/x-python-object', 'multi', token):
+            raise myWebError('404 user %s does not exist' % username)
+        COMMANDS.accessControl('setUserPassword', token=token, username=username, password=password)
         COMMANDS.save(token)
         if returnTo:
             raise web.seeother(returnTo)

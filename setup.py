c3134a58de1cf4892a757657a53846ddf49e3993
Jack Jansen <Jack.Jansen@cwi.nl>
2018-08-09 15:32:01 +0200

    Adding igorServlet to list of modules

--- setup.py
+++ setup.py
@@ -96,7 +96,7 @@ setup(
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
-       py_modules=["igorVar", "igorControl", "igorSetup", "igorCA"],
+       py_modules=["igorVar", "igorControl", "igorSetup", "igorCA", "igorServlet"],
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's

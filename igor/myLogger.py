68e474022c77e7f90c4c373a731dd81d462bedff
Jack Jansen <Jack.Jansen@cwi.nl>
2018-08-17 12:47:54 +0200

    Added flush() method to StreamToLogger

--- igor/myLogger.py
+++ igor/myLogger.py
@@ -9,14 +9,17 @@ DEBUG=False
 
 # Send stdout and stderr to the logger as well.
 class StreamToLogger(object):
-   def __init__(self, logger, log_level=logging.INFO):
-      self.logger = logger
-      self.log_level = log_level
-      self.linebuf = ''
+    def __init__(self, logger, log_level=logging.INFO):
+        self.logger = logger
+        self.log_level = log_level
+        self.linebuf = ''
 
-   def write(self, buf):
-      for line in buf.rstrip().splitlines():
-         self.logger.log(self.log_level, line.rstrip())
+    def write(self, buf):
+        for line in buf.rstrip().splitlines():
+            self.logger.log(self.log_level, line.rstrip())
+
+    def flush(self):
+        pass         
 
 def install(logSpec=None, nologfile=False, nologstderr=False, logdir='.'):
     rootLogger = logging.getLogger()

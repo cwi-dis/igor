77bd0bcc645e12ccdc860ca282f33461a6ba74d0
Jack Jansen <Jack.Jansen@cwi.nl>
2018-09-10 00:08:46 +0200

    First stab at py3 modules. xpath to be done

--- setup.py
+++ setup.py
@@ -102,7 +102,12 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=["httplib2", "requests", "web.py", "python-dateutil", "py-dom-xpath-redux", "passlib", "pyjwt", "pyopenssl"],
+    install_requires= (
+        ["httplib2", "requests", "web.py", "python-dateutil", "py-dom-xpath-redux", "passlib", "pyjwt", "pyopenssl"]
+        if sys.version_info[0] < 3 else
+        ["future", "httplib2", "requests", "web.py==0.40.dev0", "python-dateutil",  "passlib", "pyjwt", "pyopenssl"]
+        # todo: "py-dom-xpath-redux",
+        ),
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,

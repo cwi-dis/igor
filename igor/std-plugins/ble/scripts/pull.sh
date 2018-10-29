58df10d03826ef8a287a1a01ad0657d45103c3d0
Jack Jansen <Jack.Jansen@cwi.nl>
2018-10-29 23:54:22 +0100

    Converted to python-based plugin

--- igor/std-plugins/ble/scripts/pull.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ -x /usr/libexec/path_helper ]; then
-	eval `/usr/libexec/path_helper -s`
-fi
-dir=`dirname $0`
-bleURL=http://localhost:8081/
-igorVar --url $bleURL ble | igorVar --put application/json --checknonempty sensors/ble

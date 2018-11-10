665644eda018f60924a11cab1441b44bcea7c4f3
Jack Jansen <Jack.Jansen@cwi.nl>
2018-11-10 19:06:15 +0100

    Getting a bit further with iotsa discovery plugin

--- igor/std-plugins/iotsaDiscovery/igorplugin.py
+++ igor/std-plugins/iotsaDiscovery/igorplugin.py
@@ -4,9 +4,10 @@ import requests
 import os
 import iotsaControl
 import json
+import urllib
 
 from builtins import object
-class IotsaPlugin(object):
+class IotsaDiscoveryPlugin(object):
     def __init__(self, igor, pluginName, pluginData):
         self.igor = igor
         self.pluginName = pluginName
@@ -20,9 +21,45 @@ class IotsaPlugin(object):
         rv = self.wifi.findNetworks()
         return json.dumps(rv)
     
-    def findTargets(self, token=None):
-        rv = self.wifi.findNetworks()
+    def findDevices(self, token=None):
+        rv = self.wifi.findDevices()
         return json.dumps(rv)
-    
+        
+    def get(self, device, protocol="https", port=None, api="config", noverify=False, token=None, returnTo=None):
+        device = iotsaControl.api.IotsaDevice(device, protocol=protocol, port=port, noverify=(not not noverify))
+        # xxxjack need to call setBearerToken() with token for this device, if needed.
+        accessor = iotsaControl.api.IotsaConfig(device, api)
+        accessor.load()
+        rv = accessor.status
+        if returnTo:
+            queryString = urllib.parse.urlencode(rv)
+            if '?' in returnTo:
+                returnTo = returnTo + '&' + queryString
+            else:
+                returnTo = returnTo + '?' + queryString
+            return self.igor.app.raiseSeeother(returnTo)
+        return json.dumps(rv)
+        
+    def put(self, device, protocol="https", port=None, api="config", noverify=False, token=None, returnTo=None, **kwargs):
+        device = iotsaControl.api.IotsaDevice(device, protocol=protocol, port=port, noverify=(not not noverify))
+        # xxxjack need to call setBearerToken() with token for this device, if needed.
+        accessor = iotsaControl.api.IotsaConfig(device, api)
+        accessor.load()
+        for k, v in kwargs:
+            accessor.set(k, v)
+        rv = {}
+        try:
+            accessor.save()
+        except iotsaControl.api.UserIntervention as e:
+            rv['message'] = e.message
+        if returnTo:
+            queryString = urllib.parse.urlencode(rv)
+            if '?' in returnTo:
+                returnTo = returnTo + '&' + queryString
+            else:
+                returnTo = returnTo + '?' + queryString
+            return self.igor.app.raiseSeeother(returnTo)
+        return json.dumps(rv)
+        
 def igorPlugin(igor, pluginName, pluginData):
-    return IotsaPlugin(igor, pluginName, pluginData)
+    return IotsaDiscoveryPlugin(igor, pluginName, pluginData)

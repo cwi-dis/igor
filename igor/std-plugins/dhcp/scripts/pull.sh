beef158d27ff5207bc40edea214a9b4d9dcca07b
Jack Jansen <Jack.Jansen@cwi.nl>
2019-01-17 23:28:46 +0100

    Half-way point: still uses script but does install it

--- igor/std-plugins/dhcp/scripts/pull.sh
+++ igor/std-plugins/dhcp/scripts/pull.sh
@@ -2,6 +2,5 @@
 if [ -x /usr/libexec/path_helper ]; then
 	eval `/usr/libexec/path_helper -s`
 fi
-dir=`dirname $0`
 
-python $dir/dhcpleases.py --json --wrap --ping --arp -i client-hostname -i hardware -i ip-address -i alive -i arp -i ping | igorVar --put application/json sensors/dhcp
+dhcpleases --json --wrap --ping --arp -i client-hostname -i hardware -i ip-address -i alive -i arp -i ping | igorVar --put application/json sensors/dhcp

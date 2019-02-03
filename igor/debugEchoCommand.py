b592fd2c75f10b209e277757d7e7733e53e85212
Jack Jansen <Jack.Jansen@cwi.nl>
2019-02-03 22:57:57 +0100

    Removing dead wood

--- igor/debugEchoCommand.py
+++
@@ -1,7 +0,0 @@
-from __future__ import unicode_literals
-def echoCommand(*args, **kwargs):
-    rv = "Content-Type: text/plain\n\n"
-    rv += "Arguments: %s\n" % repr(args)
-    rv += "Keyword arguments: %s\n" % repr(kwargs)
-    return rv
-    
